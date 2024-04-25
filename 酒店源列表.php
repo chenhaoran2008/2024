7725d224472f2f3547a4a793a5a596266d134414
chenhaoran2008 <167660366+chenhaoran2008@users.noreply.github.com>
2024-04-26 07:31:51 +0800

    Add files via upload
    Signed-off-by: chenhaoran2008 <167660366+chenhaoran2008@users.noreply.github.com>

---
+++ 酒店源列表.php
@@ -0,0 +1,18 @@
+酒店源测试👉酒店,#genre#
+今日DJ,https://vd4.bdstatic.com/mda-pjn3sa7dmtyshen0/sc/cae_h264/1698033977064729599/mda-pjn3sa7dmtyshen0.mp4
+随州综合,http://34766.hlsplay.aodianyun.com/guangdianyun_34766/tv_channel_304.m3u8?auth_key=4836074354-0-0-d3ae3e237ae5fdaa7e72d8b930ff507b&extra_key=q_KhUzRNviTQMw35AK7VUOsxUuO3fkLbRbWjHBywJErK0zgP7mqTsFvo50t7UieqiF-pvnPCIMJa5QdRQ1gLPBYWz9HOG7TAdI5vRG232V88z98QUcOS1P6iUQbACyEaoEVKLRe_Am23Ezeahvwg0pDBetjQHrLPx7P8mro2vqw15rvUQKLSF-HBByajerez
+随州农村,http://34766.hlsplay.aodianyun.com/guangdianyun_34766/tv_channel_305.m3u8?auth_key=4834621829-0-0-49c63c7de26b595a9177e498db618f79&extra_key=xQtKveJFBYYdE5xPP5Qc5y3qlfhlj-BcHOcbWxez9NxCHHoFi0Lp44BN8aqi2_k9URCU-rsf8mQz8CYifgEaMzixzi63CT1KgCDexQ0cD5X0e0lUMZaQy0DMOXcFpW1Cf9zn7psgSNcUQiA2OGcxi7TXk-_FyDGcu767szWh8SQJ0Vr0uNRbiqE2jzk7ICiX
+支持php?ip=000.000.000.000:808这样url传参
+<!DOCTYPE html>
+<html>
+<head>
+    <title>获取酒店源列表</title>
+</head>
+<body>
+    <form method="POST" action="/gy/获取酒店源列表.php">
+        <label for="ip">输入有效酒店源的IP和端口：</label>
+        <input type="text" id="ip" name="ip">
+        <input type="submit" value="查询">
+    </form>
+</body>
+</html>
